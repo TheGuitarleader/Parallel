760771f218e579afeec4119354791af1f795e496
Kyle | Guitar <26614720+TheGuitarleader@users.noreply.github.com>
2025-07-09 14:58:31 -0500

    Added projects to solution

---
+++ Parallel.Cli/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Parallel.Cli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
