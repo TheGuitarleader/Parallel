65b4064dff3528bddef0eb24daafba6f2117cf18
Kyle | Guitar <26614720+TheGuitarleader@users.noreply.github.com>
2025-07-09 19:32:44 -0500

    Added service host project files

---
+++ Parallel.Service/Responses/IServiceResponse.cs
@@ -0,0 +1,9 @@
+﻿// Copyright 2025 Kyle Ebbinga
+
+namespace Parallel.Service.Responses
+{
+    public interface IServiceResponse
+    {
+        
+    }
+}
---
+++ Parallel.Service/Services/TcpRequestService.cs
@@ -0,0 +1,14 @@
+﻿// Copyright 2025 Kyle Ebbinga
+
+using Microsoft.Extensions.Hosting;
+
+namespace Parallel.Service.Services
+{
+    public class TcpRequestService : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
