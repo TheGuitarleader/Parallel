7dfae428111d6947969af8210ba8183b8d85bf86
Kyle | Guitar <26614720+TheGuitarleader@users.noreply.github.com>
2025-07-23 22:06:03 -0500

    Renamed Parallel Server to Parallel Service

---
+++ Parallel.Service/Requests/HelpRequest.cs
@@ -0,0 +1,17 @@
+﻿// Copyright 2025 Kyle Ebbinga
+
+using System.ComponentModel;
+using Parallel.Core.Net.Sockets;
+using Parallel.Service.Responses;
+
+namespace Parallel.Service.Requests
+{
+    [Description("Lists all avalible requests to the server.")]
+    public class HelpRequest : BaseRequest
+    {
+        public override Task<IResponse> ExecuteAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Parallel.Service/Responses/IResponse.cs
@@ -0,0 +1,8 @@
+﻿// Copyright 2025 Kyle Ebbinga
+
+namespace Parallel.Service.Responses
+{
+    public interface IResponse
+    {
+    }
+}
