53d9965498c633762a465c7104f4d16bc71566ed
Kyle | Guitar <26614720+TheGuitarleader@users.noreply.github.com>
2025-11-11 06:28:53 -0600

    Added build script

---
+++ Build-Release.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set SCRIPT_DIR=%~dp0
+set BUILDS_DIR="%SCRIPT_DIR%Builds"
+
+echo Building to folder: %BUILDS_DIR%
+rd /s /q "%BUILDS_DIR%"
+
+echo Building Parallel.Cli...
+CALL dotnet publish "%SCRIPT_DIR%Parallel.Cli\Parallel.Cli.csproj" -r win-x64 -c Release /p:PublishSingleFile=true /p:PublishDir="%BUILDS_DIR%/win-x64"
+CALL dotnet publish "%SCRIPT_DIR%Parallel.Cli\Parallel.Cli.csproj" -r osx-x64 -c Release /p:PublishSingleFile=true /p:PublishDir="%BUILDS_DIR%/osx-x64"
+CALL dotnet publish "%SCRIPT_DIR%Parallel.Cli\Parallel.Cli.csproj" -r linux-x64 -c Release /p:PublishSingleFile=true /p:PublishDir="%BUILDS_DIR%/linux-x64"
+CALL dotnet publish "%SCRIPT_DIR%Parallel.Cli\Parallel.Cli.csproj" -r linux-arm -c Release /p:PublishSingleFile=true /p:PublishDir="%BUILDS_DIR%/linux-arm"
