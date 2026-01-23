94a7f5037cc6eff0b2615a18131b5c0ca7de718e
Kyle | Guitar <26614720+TheGuitarleader@users.noreply.github.com>
2026-01-23 09:05:38 -0600

    Added SignalR logging framework for sending logs to the GUI

---
+++ Parallel.Service/Extensions/Logging/SignalRLoggerProvider.cs
@@ -0,0 +1,35 @@
+﻿// Copyright 2026 Kyle Ebbinga
+
+using Microsoft.AspNetCore.SignalR;
+using Parallel.Service.Services;
+
+namespace Parallel.Service.Extensions.Logging
+{
+    [ProviderAlias("SignalRLogger")]
+    public class SignalRLoggerProvider : ILoggerProvider, ISupportExternalScope
+    {
+        private IExternalScopeProvider _scopeProvider = default!;
+        private readonly IServiceProvider _services;
+
+        public SignalRLoggerProvider(IServiceProvider services)
+        {
+            _services = services;
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new SignalRLogger(
+                categoryName,
+                _scopeProvider,
+                _services
+            );
+        }
+
+        public void SetScopeProvider(IExternalScopeProvider scopeProvider)
+        {
+            _scopeProvider = scopeProvider;
+        }
+
+        public void Dispose() { }
+    }
+}
