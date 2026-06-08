e5efd16487b56e178876191dcd501f24a25f63c1
Kyle Ebbinga <26614720+TheGuitarleader@users.noreply.github.com>
2026-06-07 20:36:54 -0500

    Update error message for release download failure

--- install.sh
+++ install.sh
@@ -3,9 +3,15 @@ set -e
 
 REPO="TheGuitarleader/Parallel"
 ASSET_PATTERN="Parallel-.*-cmd-linux-x64.zip"
-DOWNLOAD_URL=$(curl -s "https://api.github.com/repos/$REPO/releases/latest" | grep -E "browser_download_url.*$ASSET_PATTERN" | cut -d '"' -f 4)
+INSTALL_ROOT="/usr/local/lib/parallel"
+BIN_LINK="/usr/local/bin/parallel"
+TMP_DIR="$(mktemp -d)"
+ZIP_FILE="$TMP_DIR/parallel.zip"
 
+DOWNLOAD_URL=$(curl -s "https://api.github.com/repos/$REPO/releases/latest" | grep -E "browser_download_url.*$ASSET_PATTERN" | cut -d '"' -f 4)
 [[ -z "$DOWNLOAD_URL" ]] && {
-  echo "Could not find a current release."
+  echo "Could not find a release."
   exit 1
 }
+
+echo "Downloading $DOWNLOAD_URL"
