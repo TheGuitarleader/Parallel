abd1ecf97737ab6c1fbe40f312ab94c9fed48942
TheGuitarleader <26614720+TheGuitarleader@users.noreply.github.com>
2026-06-12 00:31:37 -0500

    Adding logging to desktop app

--- Parallel.Desktop/Program.cs
+++ Parallel.Desktop/Program.cs
@@ -1,5 +1,11 @@
 ﻿using Avalonia;
 using System;
+using System.IO;
+using System.Reflection;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
+using Parallel.Core.IO;
+using Parallel.Core.Settings;
 
 namespace Parallel.Desktop;
 
@@ -9,13 +15,27 @@ sealed class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        AssemblyName assembly = Assembly.GetExecutingAssembly().GetName();
+        
+        string logDir = Path.Combine(PathBuilder.TempDirectory, "Logs");
+        if (!Directory.Exists(logDir)) Directory.CreateDirectory(logDir);
+        
+        Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo.File(Path.Combine(logDir, $"{assembly.Name}.log")).WriteTo.Console().CreateLogger();
+        Log.Information($"{assembly.Name} [Version {assembly.Version}]");
+        
+        if (!ParallelConfig.CanStartNewInstance())
+        {
+            Log.Warning("Can't start new instance of Parallel!");
+            
+            //MessageBoxManager.GetMessageBoxStandard("Warning", "An instance of Parallel is already running!", ButtonEnum.Ok, Icon.Warning);
+            return;
+        }
+        
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
-    public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
-            .UsePlatformDetect()
-            .WithInterFont()
-            .LogToTrace();
+    public static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<App>().UsePlatformDetect().WithInterFont().LogToTrace();
 }
